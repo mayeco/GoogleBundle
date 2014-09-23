db7a485fc42db1fab1d05d801265045ca483ab0b
Mario Young <maye.co@gmail.com>
2014-09-23 14:53:38 -0300

    reformat

--- Services/GoogleUtils.php
+++ Services/GoogleUtils.php
@@ -4,97 +4,97 @@ namespace Mayeco\GoogleBundle\Services;
 
 use Lsw\MemcacheBundle\Cache\AntiDogPileMemcache;
 
-class GoogleUtils 
+class GoogleUtils
 {
 
-	protected $adwordsuser;
-	protected $adwordsversion;
-	protected $apiclient;
-	protected $memcache;
+    protected $adwordsuser;
+    protected $adwordsversion;
+    protected $apiclient;
+    protected $memcache;
 
     public function __construct(\AdWordsUser $adwordsuser, \Google_Client $apiclient, AntiDogPileMemcache $memcache, $adwordsversion) {
-    
+
         $this->adwordsuser = $adwordsuser;
         $this->apiclient = $apiclient;
         $this->adwordsversion = $adwordsversion;
         $this->memcache = $memcache;
     }
-    
+
     public function ConvertMicrosInCsvDataReport($csvdata, $title = null) {
-        
+
         //inicia conversion micros
-		$count = 0;
-		$costpos = array();
-		$tmpfname = tempnam(sys_get_temp_dir(), "ZZZ");
-		$outstream = fopen($tmpfname, "r+");
-		foreach(preg_split("/((\r?\n)|(\r\n?))/", $csvdata) as $line){
-			
-			if($count == 0) {
-				$count++;
-				if($title) {
-				    fputcsv($outstream, array($title));
-				}
+        $count = 0;
+        $costpos = array();
+        $tmpfname = tempnam(sys_get_temp_dir(), "ZZZ");
+        $outstream = fopen($tmpfname, "r+");
+        foreach(preg_split("/((\r?\n)|(\r\n?))/", $csvdata) as $line){
 
-				continue;
-			}
-			
-			$linearray = str_getcsv($line);
+            if($count == 0) {
+                $count++;
+                if($title) {
+                    fputcsv($outstream, array($title));
+                }
 
-			if($count == 1) {
-				//this is header
-				$postvalue = 0;
+                continue;
+            }
 
-				foreach($linearray as $value){
-					if (strpos($value,'Cost') !== false) {
-						$costpos[] = $postvalue;
-					}
-					$postvalue++;
-				}
+            $linearray = str_getcsv($line);
 
-				fputcsv($outstream, $linearray);
-				$count++;
-				continue;
-			}
-			
-			if(!empty($costpos)) {
+            if($count == 1) {
+                //this is header
+                $postvalue = 0;
 
-				foreach($costpos as $costpostval){
+                foreach($linearray as $value){
+                    if (strpos($value,'Cost') !== false) {
+                        $costpos[] = $postvalue;
+                    }
+                    $postvalue++;
+                }
 
-					if(isset($linearray[$costpostval])) {
-						$linearray[$costpostval] = $linearray[$costpostval] / 1000000;
-					}
+                fputcsv($outstream, $linearray);
+                $count++;
+                continue;
+            }
 
-				}
-			}
+            if(!empty($costpos)) {
 
-			fputcsv($outstream, $linearray);
-			$count++;
-		}
-		
+                foreach($costpos as $costpostval){
+
+                    if(isset($linearray[$costpostval])) {
+                        $linearray[$costpostval] = $linearray[$costpostval] / 1000000;
+                    }
+
+                }
+            }
+
+            fputcsv($outstream, $linearray);
+            $count++;
+        }
+
         fclose($outstream);
-        
+
         return $tmpfname;
     }
 
 
     public function DownloadReportWithAwql($awql, $format="CSV") {
 
-		if(!$this->ValidateAdwordsOAuth2Info())
-			return;
-	
-		$report = null;
-		try {
+        if(!$this->ValidateAdwordsOAuth2Info())
+            return;
+
+        $report = null;
+        try {
 
             $options = array('version' => $this->adwordsversion);
             $report = \ReportUtils::DownloadReportWithAwql($awql, NULL, $this->adwordsuser, $format, $options);
-			
-		} catch (\Exception $e) {
-		
-			return;
-		}
-		
-		return $report;
-	}
+
+        } catch (\Exception $e) {
+
+            return;
+        }
+
+        return $report;
+    }
 
     public function setAdwordsOAuth2Validate($refreshToken, $accessToken) {
 
@@ -107,71 +107,71 @@ class GoogleUtils
         return $this->ValidateAdwordsOAuth2Info();
     }
 
-	public function ValidateAdwordsOAuth2Info() {
-	
-		try {
-		
-			$this->adwordsuser->ValidateOAuth2Info();
-			
-		} catch (\Exception $e) {
-		
-			return;
-		}
+    public function ValidateAdwordsOAuth2Info() {
 
-		return true;
-	}
-	
-	public function GetAdwordsService($service) {
-	
-		if(!$this->ValidateAdwordsOAuth2Info())
-			return;
-
-		
-		try {
-		
-			$service = $this->adwordsuser->GetService($service, $this->adwordsversion);
-			
-		} catch (\Exception $e) {
-		
-			return;
-		}
-
-		return $service;
-	}
-	
-	public function GetAdwordsUser() {
-	
-		if(!$this->ValidateAdwordsOAuth2Info())
-			return;
-
-		return $this->adwordsuser;
-	}
-
-	public function GetGoogleApi() {
-	
-		return $this->apiclient;
-	}
-
-	public function createAuthUrl() {
-	
-		return $this->apiclient->createAuthUrl();
-	}
-
-	public function authenticate($code) {
-	
         try {
-                
+
+            $this->adwordsuser->ValidateOAuth2Info();
+
+        } catch (\Exception $e) {
+
+            return;
+        }
+
+        return true;
+    }
+
+    public function GetAdwordsService($service) {
+
+        if(!$this->ValidateAdwordsOAuth2Info())
+            return;
+
+
+        try {
+
+            $service = $this->adwordsuser->GetService($service, $this->adwordsversion);
+
+        } catch (\Exception $e) {
+
+            return;
+        }
+
+        return $service;
+    }
+
+    public function GetAdwordsUser() {
+
+        if(!$this->ValidateAdwordsOAuth2Info())
+            return;
+
+        return $this->adwordsuser;
+    }
+
+    public function GetGoogleApi() {
+
+        return $this->apiclient;
+    }
+
+    public function createAuthUrl() {
+
+        return $this->apiclient->createAuthUrl();
+    }
+
+    public function authenticate($code) {
+
+        try {
+
             $jsontoken = $this->apiclient->authenticate($code);
 
         } catch (\Exception $e) {
-                
+
             return;
         }
 
         $fulltoken = json_decode($jsontoken, true);
         if(!isset($fulltoken["access_token"]) || empty(trim($fulltoken["access_token"])))
             return;
-               
+
         $q = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" . $fulltoken["access_token"];
         $json = file_get_contents($q);
         if(false === $json)
@@ -182,48 +182,48 @@ class GoogleUtils
             return;
 
         $this->memcache->set($userinfo['id'] . '_token', $jsontoken, $fulltoken["expires_in"] - 30);
-            
+
         return array("jsontoken" => $jsontoken, "fulltoken" => $fulltoken, "userinfo" => $userinfo);
     }
 
     public function Relogin($googleid, $refreshToken) {
 
         if( !$jsontoken = $this->memcache->get($googleid . '_token')  ) {
-                
+
             try {
-                
+
                 $this->apiclient->refreshToken($refreshToken);
-                
+
             } catch (\Exception $e) {
 
                 return;
             }
-                
+
             $jsontoken = $this->apiclient->getAccessToken();
             $fulltoken = json_decode($jsontoken, true);
-                
+
             $this->memcache->set($googleid . '_token', $jsontoken, $fulltoken["expires_in"] - 30);
         }
-        
-        
+
+
         try {
-            
+
             $this->apiclient->setAccessToken($jsontoken);
-                
+
         } catch (\Exception $e) {
 
             return;
         }
-        
-        
+
+
         if($this->apiclient->isAccessTokenExpired()) {
-                
-            return;    
+
+            return;
         }
 
         $fulltoken = json_decode($jsontoken, true);
         if(!isset($fulltoken["access_token"]) || empty(trim($fulltoken["access_token"]))) {
-            
+
             return;
         }
 
@@ -273,7 +273,7 @@ class GoogleUtils
         return $adgrouparray;
 
     }
-    
+
     public function GetCampaigns() {
 
         $campaignService = $this->GetAdwordsService('CampaignService');
