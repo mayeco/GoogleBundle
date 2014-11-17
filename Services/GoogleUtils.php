0262669875f21bb0f51470a22e6c7f74d0b15fe6
Mario Young <maye.co@gmail.com>
2014-11-17 03:25:57 -0300

    Update GoogleUtils.php

--- Services/GoogleUtils.php
+++ Services/GoogleUtils.php
@@ -171,18 +171,19 @@ class GoogleUtils
 
             $this->apiclient->setAccessToken($jsontoken);
             
+            if(!$this->setAdwordsOAuth2Validate($refreshToken, $fulltoken["access_token"])) {
+                return;
+            }
+
             $service = new \Google_Service_Oauth2($this->apiclient);
             $service->tokeninfo(
                 array(
                     "access_token" => $fulltoken["access_token"]
                 )
             );
-            
-            if(!$this->setAdwordsOAuth2Validate($refreshToken, $fulltoken["access_token"]))
-                return;
 				
         } catch (\Exception $e) {
-
+            
             return;
         }
 
