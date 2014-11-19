2f629cfa1200e3d1340e2bdd3476501715f1a6b2
Mario Young <maye.co@gmail.com>
2014-11-19 02:54:23 -0300

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('google');
+        $rootNode = $treeBuilder->root('mayeco_google');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
