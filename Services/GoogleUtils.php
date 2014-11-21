bd411a63ce826ab3272abc4a64ccd7df4b25895c
Mario Young <maye.co@gmail.com>
2014-11-21 13:09:56 -0300

    format

--- Services/GoogleUtils.php
+++ Services/GoogleUtils.php
@@ -6,52 +6,81 @@ use Google_Client;
 use AdWordsUser;
 use Lsw\MemcacheBundle\Cache\MemcacheInterface;
 
+/**
+ * Class GoogleUtils
+ * @package Mayeco\GoogleBundle\Services
+ */
 class GoogleUtils
 {
 
+    /**
+     * @var AdWordsUser
+     */
     protected $adwordsuser;
+    /**
+     * @var Google_Client
+     */
     protected $apiclient;
+    /**
+     * @var MemcacheInterface
+     */
     protected $memcache;
 
+    /**
+     * @param AdWordsUser $adwordsuser
+     * @param Google_Client $apiclient
+     * @param MemcacheInterface $memcache
+     */
     public function __construct(
-        AdWordsUser $adwordsuser, 
-        Google_Client $apiclient, 
+        AdWordsUser $adwordsuser,
+        Google_Client $apiclient,
         MemcacheInterface $memcache
-    ) {
-
+    )
+    {
         $this->adwordsuser = $adwordsuser;
         $this->apiclient = $apiclient;
         $this->memcache = $memcache;
     }
-    
-    public function DownloadReportWithAwql($reportQuery, $format="CSV", array $options = NULL) {
-        
+
+    /**
+     * @param $reportQuery
+     * @param string $format
+     * @param array $options
+     * @return null|string|void
+     */
+    public function DownloadReportWithAwql($reportQuery, $format = "CSV", array $options = NULL)
+    {
         $allowformats = array("CSV", "XML", "TSV", "GZIPPED_CSV", "GZIPPED_XML");
         if (!in_array($format, $allowformats))
             return;
-        
-        if(!$this->ValidateUser())
+
+        if (!$this->ValidateUser())
             return;
-        
+
         $report = null;
         try {
-            
+
             $report = \ReportUtils::DownloadReportWithAwql($reportQuery, null, $this->adwordsuser, $format, $options);
-            
-            if("GZIPPED_CSV" == $format || "GZIPPED_XML" == $format)
-                $report = gzdecode ($report);
+
+            if ("GZIPPED_CSV" == $format || "GZIPPED_XML" == $format)
+                $report = gzdecode($report);
 
         } catch (\Exception $e) {
-            
+
             return;
         }
-        
+
         return $report;
     }
-    
-    public function setAdwordsOAuth2Validate($fulltoken) {
-        
-        if(!isset($fulltoken["access_token"]) || !isset($fulltoken["refresh_token"])) {
+
+    /**
+     * @param $fulltoken
+     * @return bool|void
+     * @throws \Exception
+     */
+    public function setAdwordsOAuth2Validate($fulltoken)
+    {
+        if (!isset($fulltoken["access_token"]) || !isset($fulltoken["refresh_token"])) {
             throw new \Exception('No access token or refresh token.');
         }
 
@@ -64,8 +93,11 @@ class GoogleUtils
         return $this->ValidateUser();
     }
 
-    public function ValidateUser() {
-
+    /**
+     * @return bool|void
+     */
+    public function ValidateUser()
+    {
         try {
 
             $this->adwordsuser->ValidateUser();
@@ -78,11 +110,14 @@ class GoogleUtils
         return true;
     }
 
-    public function GetAdwordsService($service) {
-
-        if(!$this->ValidateUser())
+    /**
+     * @param $service
+     */
+    public function GetAdwordsService($service)
+    {
+        if (!$this->ValidateUser())
             return;
-            
+
         try {
 
             $service = $this->adwordsuser->GetService($service);
@@ -95,28 +130,44 @@ class GoogleUtils
         return $service;
     }
 
-    public function GetAdwordsUser() {
-
+    /**
+     * @return AdWordsUser
+     */
+    public function GetAdwordsUser()
+    {
         return $this->adwordsuser;
     }
 
-    public function GetGoogleApi() {
-
+    /**
+     * @return Google_Client
+     */
+    public function GetGoogleApi()
+    {
         return $this->apiclient;
     }
 
-    public function createAuthUrl() {
-        
+    /**
+     * @return mixed
+     */
+    public function createAuthUrl()
+    {
         return $this->apiclient->createAuthUrl();
     }
 
-    public function setAdwordsId($adwordsid) {
-        
+    /**
+     * @param $adwordsid
+     */
+    public function setAdwordsId($adwordsid)
+    {
         $this->adwordsuser->SetClientCustomerId($adwordsid);
     }
 
-    public function Authenticate($code) {
-
+    /**
+     * @param $code
+     * @return array|void
+     */
+    public function Authenticate($code)
+    {
         try {
 
             $jsontoken = $this->apiclient->authenticate($code);
@@ -130,36 +181,41 @@ class GoogleUtils
 
             return;
         }
-        
+
         $this->memcache->set($user_id . '_token', $jsontoken, $fulltoken["expires_in"] - 60);
-        
+
         return array(
-            "user_id" => $user_id, 
-            "access_token" => $fulltoken["access_token"], 
-            "refresh_token" => $fulltoken["refresh_token"], 
-            "expires_in" => $fulltoken["expires_in"], 
+            "user_id" => $user_id,
+            "access_token" => $fulltoken["access_token"],
+            "refresh_token" => $fulltoken["refresh_token"],
+            "expires_in" => $fulltoken["expires_in"],
         );
     }
 
-    public function RefreshAccess($id, $refreshToken) {
-
-        if( !$jsontoken = $this->memcache->get($id . '_token')  ) {
+    /**
+     * @param $id
+     * @param $refreshToken
+     * @return array|void
+     */
+    public function RefreshAccess($id, $refreshToken)
+    {
+        if (!$jsontoken = $this->memcache->get($id . '_token')) {
 
             try {
 
                 $this->apiclient->refreshToken($refreshToken);
                 $verify_token = $this->apiclient->verifyIdToken();
-                if($verify_token->getUserId() != $id)
+                if ($verify_token->getUserId() != $id)
                     return;
-                
+
                 $jsontoken = $this->apiclient->getAccessToken();
-                
+
             } catch (\Exception $e) {
 
                 $this->memcache->delete($id . '_token');
                 return;
             }
-            
+
             $fulltoken = json_decode($jsontoken, true);
             $this->memcache->set($id . '_token', $jsontoken, $fulltoken["expires_in"] - 60);
         }
@@ -178,22 +234,22 @@ class GoogleUtils
                     "access_token" => $fulltoken["access_token"]
                 )
             );
-				
+
         } catch (\Exception $e) {
-            
+
             $this->memcache->delete($id . '_token');
             return;
         }
 
         return array(
-            "accessType" => $tokeninfo->accessType, 
-            "audience" => $tokeninfo->audience, 
-            "email" => $tokeninfo->email, 
-            "expiresIn" => $tokeninfo->expiresIn, 
-            "issuedTo" => $tokeninfo->issuedTo, 
-            "scope" => $tokeninfo->scope, 
-            "userId" => $tokeninfo->userId, 
-            "verifiedEmail" => $tokeninfo->verifiedEmail, 
+            "accessType" => $tokeninfo->accessType,
+            "audience" => $tokeninfo->audience,
+            "email" => $tokeninfo->email,
+            "expiresIn" => $tokeninfo->expiresIn,
+            "issuedTo" => $tokeninfo->issuedTo,
+            "scope" => $tokeninfo->scope,
+            "userId" => $tokeninfo->userId,
+            "verifiedEmail" => $tokeninfo->verifiedEmail,
         );
     }
 
